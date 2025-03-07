ad61aa1f62e6c4897a6b9cf3877476a18a3d4bd4
emilvdijk <emilvdijk@gmail.com>
2025-03-07 09:50:57 +0100

    made it not a war program but jar

--- chat/build.gradle.kts
+++ chat/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     java
-    war
-    id("org.springframework.boot") version "3.4.3"
+        id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "nl.emilvdijk"
-version = "0.0.1-SNAPSHOT"
+version = "1.0.0"
 
 java {
     toolchain {
@@ -29,7 +28,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-websocket")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
-    providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
